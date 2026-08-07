7131552e55fa4316f36f91405d41836a57315a47
MyF-delivery <abiyfrity@gmail.com>
2026-08-07 17:04:40 +0300

    Delete env.sh

--- env.sh
+++
@@ -1,6 +0,0 @@
-# Point your domain's DNS A record to your server IP
-# Example: mbingo.com -> 123.45.67.89
-
-# Update the .env file with your domain
-nano /var/www/m-bingo/backend/.env
-# Change DOMAIN to https://your-domain.com
