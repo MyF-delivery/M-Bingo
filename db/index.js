f2eaba6c8f20151e16fd1ba45b36468f80530bd6
MyF-delivery <abiyfrity@gmail.com>
2026-08-26 16:10:47 +0300

    Update index.js

--- db/index.js
+++ db/index.js
@@ -1,4 +1,17 @@
+
+// db/index.js
 const { Pool } = require('pg');
 const config = require('../config/env');
-const pool = new Pool({ ...config.db });
+
+const pool = new Pool({
+  host: config.DB_HOST,
+  port: config.DB_PORT,
+  database: config.DB_NAME,
+  user: config.DB_USER,
+  password: config.DB_PASSWORD,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 5000,
+});
+
 module.exports = pool;
